e7b95eba77538a7e254e54a3beec9d1e19af3fd7
highscopeDER <61238297+highscopeDER@users.noreply.github.com>
2020-03-09 12:21:34 +0900

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,10 +1,8 @@
-package com.example.smartcount
-
-
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
+import android.util.Log
+import android.widget.LinearLayout
 import androidx.appcompat.app.AlertDialog
 import androidx.core.view.isVisible
 import kotlin.math.*
@@ -19,6 +17,18 @@ class  MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        settBut.setOnClickListener {
+
+            val settframe = AlertDialog.Builder(this)
+            settframe.setTitle("Settings")
+            val settframeview = getLayoutInflater()
+                .inflate(R.layout.settings, null)
+            settframe.setView(settframeview)
+            settframe.show()
+            settframe.setCancelable(true)
+        }
+
         val alert = AlertDialog.Builder(this)
         alert.setTitle("Answer")
         alert.setCancelable(true)
@@ -32,24 +42,24 @@ class  MainActivity : AppCompatActivity() {
            dialog.dismiss()
            when{
                which == 0 -> {
-                   numA.isVisible = true; charA.isVisible = true
-                   numB.isVisible = true; charB.isVisible = true
-                   numC.isVisible = true; charC.isVisible = true
+                   numA.isVisible = true
+                   numB.isVisible = true
+                   numC.isVisible = true
                }
                which == 1 -> {
-                   numA.isVisible = false; charA.isVisible = false
-                   numB.isVisible = true; charB.isVisible = true
-                   numC.isVisible = true; charC.isVisible = true
+                   numA.isVisible = false
+                   numB.isVisible = true
+                   numC.isVisible = true
                }
                which == 2 -> {
-                   numA.isVisible = true; charA.isVisible = true
-                   numB.isVisible = false; charB.isVisible = false
-                   numC.isVisible = true; charC.isVisible = true
+                   numA.isVisible = true
+                   numB.isVisible = false
+                   numC.isVisible = true
                }
                which == 3 -> {
-                   numA.isVisible = true; charA.isVisible = true
-                   numB.isVisible = true; charB.isVisible = true
-                   numC.isVisible = false; charC.isVisible = false
+                   numA.isVisible = true
+                   numB.isVisible = true
+                   numC.isVisible = false
                }
            }
 
@@ -59,28 +69,20 @@ class  MainActivity : AppCompatActivity() {
             ChangeEqForm.show()
         }
 
-
-
         fun  Solve_0(){
 
+            val A = TurnInto(numA.text!!)
+            val B = TurnInto(numB.text!!)
+            val C = TurnInto(numC.text!!)
 
-            var A = TurnInto(numA.text!!)
-            var B = TurnInto(numB.text!!)
-            var C = TurnInto(numC.text!!)
-
-
-            when {
-                charA.isChecked == false -> A = -A
-                charB.isChecked == false -> B = -B
-                charC.isChecked == false -> C = -C
-            }
-            val disc: Double = B * B + (- 4 * A * C)
+            val disc: Double = (B * B) + (-4 * A * C)
+            val coren: Double = sqrt(disc)
             val x1: Double
             val x2: Double
 
             if(disc > 0){
-                x1 = (-B + sqrt(disc)) / 2 * A
-                x2 = (-B - sqrt(disc)) / 2 * A
+                x1 = (-B + coren) / (2 * A)
+                x2 = (-B - coren) / (2 * A)
                 alert.setMessage("x1 = $x1, x2 = $x2")
             }
 
@@ -95,46 +97,32 @@ class  MainActivity : AppCompatActivity() {
                 alert.setMessage("No answer because discriminant is less than zero")
             }
             alert.show()
-
             }
+
         fun Solve_1(){
-            var B = TurnInto(numB.text!!)
-            var C = TurnInto(numC.text!!)
+            val B = TurnInto(numB.text!!)
+            val C = TurnInto(numC.text!!)
 
-            when {
-                charB.isChecked == false -> B = -B
-                charC.isChecked == false -> C = -C
-            }
             val x = -C / B
             alert.setMessage("x = $x")
             alert.show()
         }
         fun Solve_2(){
-            var A = TurnInto(numA.text!!)
-            var C = TurnInto(numC.text!!)
+            val A = TurnInto(numA.text!!)
+            val C = TurnInto(numC.text!!)
 
-            when {
-                charA.isChecked == false -> A = -A
-                charC.isChecked == false -> C = -C
-            }
             val x1 = sqrt(-C / A)
             val x2 = -x1
             alert.setMessage("x1 = $x1, x2 = $x2 ")
             alert.show()
         }
         fun Solve_3(){
-            var A = TurnInto(numA.text!!)
-            var B = TurnInto(numB.text!!)
-
-            when {
-                charA.isChecked == false -> A = -A
-                charB.isChecked == false -> B = -B
-            }
+            val A = TurnInto(numA.text!!)
+            val B = TurnInto(numB.text!!)
 
             val x2 = -B / A
             alert.setMessage("x1 = 0, x2 = $x2")
             alert.show()
-
         }
 
         solveBut.setOnClickListener {
@@ -162,9 +150,7 @@ class  MainActivity : AppCompatActivity() {
             numA.text.clear()
             numB.text.clear()
             numC.text.clear()
-            charA.isChecked = true
-            charB.isChecked = true
-            charC.isChecked = true
+
         }
 
         fun TurnInto(value: Editable): Double{
